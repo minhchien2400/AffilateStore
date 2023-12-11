0920cd72a857b5ce28a6a1dbfedbc908ff75c8bb
Leon <leon.tran@avepoint.com>
2023-12-11 20:52:22 +0700

    ADD

---
+++ AffiliateStoreBE/AffiliateStoreBE/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using AffiliateStoreBE.Models;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace AffiliateStoreBE.Configurations
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.ToTable("refresh-tokens");
+            builder.HasKey(a => a.Id);
+            builder.Property(a => a.Id).ValueGeneratedOnAdd();
+            builder.Property(a => a.RefreshTokenStr).IsRequired();
+            builder.Property(a => a.ExpireDate).IsRequired();
+            builder.Property(a => a.AccountId).IsRequired();
+            builder.HasOne(a => a.Account).WithMany().HasForeignKey(a => a.AccountId).OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ AffiliateStoreBE/AffiliateStoreBE/Models/RefreshToken.cs
@@ -0,0 +1,11 @@
+﻿namespace AffiliateStoreBE.Models
+{
+    public class RefreshToken
+    {
+        public Guid Id { get; set; }    
+        public string RefreshTokenStr { get; set; }
+        public DateTime ExpireDate { get; set; }
+        public string AccountId { get; set; }
+        public Account Account { get; set; }
+    }
+}
